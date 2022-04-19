bb0534c0a0073116607861e48832828c196940f7
Soun9922 <sagsounak39@gmail.com>
2022-04-19 23:55:37 +0530

    this file is under reviewing process as error occurs everytime

--- print20marchto20juneif.sh
+++ print20marchto20juneif.sh
@@ -3,7 +3,7 @@
 read -p "Enter the date:" d
 read -p "Enter the month:" m
 
-if [ $m -le 6 -a $d -le 20 -a $m -ge 3 -a $d -le 20 -a $d -lt 31 ]
+if [ $m -ge 3 -a $d -ge 31 -a $m -le 6 -a $d -le 30 -a $d -lt 31 ]
 then
 		echo $m $d "True"
 else
